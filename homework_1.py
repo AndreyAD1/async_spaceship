e7cad95ad9020eabd0712bd39959c93a2879326d
Andrey Arakelyants <andrey.arakelyants@myoffice.team>
2020-07-23 00:08:31 +0300

    Make asynchronous blinks.

--- homework_1.py
+++ homework_1.py
@@ -8,11 +8,11 @@ TIC_TIMEOUT = 0.1
 
 
 async def blink(canvas, row, column, symbol='*'):
-    while True:
-        canvas.addstr(row, column, symbol, curses.A_DIM)
-        for _ in range(20):
-            await asyncio.sleep(0)
+    canvas.addstr(row, column, symbol, curses.A_DIM)
+    for _ in range(random.randint(1, 30)):
+        await asyncio.sleep(0)
 
+    while True:
         canvas.addstr(row, column, symbol)
         for _ in range(3):
             await asyncio.sleep(0)
@@ -23,6 +23,10 @@ async def blink(canvas, row, column, symbol='*'):
 
         canvas.addstr(row, column, symbol)
         for _ in range(3):
+            await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol, curses.A_DIM)
+        for _ in range(20):
             await asyncio.sleep(0)
 
 

6de39e91abc1b192513610962b2596598befe8c4
Andrey Arakelyants <andrey.arakelyants@myoffice.team>
2020-07-23 10:30:47 +0300

    Add the shot animation.

--- homework_1.py
+++ homework_1.py
@@ -30,6 +30,36 @@ async def blink(canvas, row, column, symbol='*'):
             await asyncio.sleep(0)
 
 
+async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0):
+    """Display animation of gun shot, direction and speed can be specified."""
+
+    row, column = start_row, start_column
+
+    canvas.addstr(round(row), round(column), '*')
+    await asyncio.sleep(0)
+
+    canvas.addstr(round(row), round(column), 'O')
+    await asyncio.sleep(0)
+    canvas.addstr(round(row), round(column), ' ')
+
+    row += rows_speed
+    column += columns_speed
+
+    symbol = '-' if columns_speed else '|'
+
+    rows, columns = canvas.getmaxyx()
+    max_row, max_column = rows - 1, columns - 1
+
+    curses.beep()
+
+    while 0 < row < max_row and 0 < column < max_column:
+        canvas.addstr(round(row), round(column), symbol)
+        await asyncio.sleep(0)
+        canvas.addstr(round(row), round(column), ' ')
+        row += rows_speed
+        column += columns_speed
+
+
 def get_stars(canvas, line_number, column_number):
     window_square = line_number * column_number
     stars = []
@@ -53,13 +83,16 @@ def draw(canvas):
     curses.curs_set(False)
     line_number, column_number = canvas.getmaxyx()
     stars = get_stars(canvas, line_number, column_number)
+    shot = fire(canvas, line_number / 2, column_number / 2)
+    coroutines = [shot, *stars]
 
     while True:
-        for s in stars:
+        for s in coroutines.copy():
             try:
                 s.send(None)
             except StopIteration:
-                continue
+                coroutines.remove(s)
+                canvas.border()
         canvas.refresh()
         time.sleep(TIC_TIMEOUT)
 

35321d8e482bb4b8e32104ab5b0b38db5e124774
Andrey Arakelyants <arakelyants.andrey@yandex.ru>
2020-07-25 18:15:01 +0300

    Fix the starship animation.

--- main.py
+++ main.py
@@ -65,7 +65,6 @@ async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0
 
 
 async def animate_spaceship(canvas, start_row, start_column, animation_frames):
-    cancellation_number = 0
     for frame in cycle(animation_frames):
         try:
             draw_frame(canvas, start_row, start_column, frame)
@@ -73,9 +72,7 @@ async def animate_spaceship(canvas, start_row, start_column, animation_frames):
             draw_frame(canvas, start_row, start_column, frame, negative=True)
         except asyncio.CancelledError:
             draw_frame(canvas, start_row, start_column, frame, negative=True)
-            cancellation_number += 1
-            if cancellation_number == 2:
-                break
+            break
 
 
 def get_stars(canvas, line_number, column_number):
@@ -129,7 +126,6 @@ def draw(canvas):
                 coroutines.remove(s)
                 canvas.border()
 
-        spaceship.send(None)
         rows_dir, columns_dir, space_pressed = read_controls(canvas)
         if rows_dir or columns_dir:
             line += rows_dir
@@ -137,8 +133,15 @@ def draw(canvas):
             try:
                 spaceship.throw(asyncio.CancelledError)
             except StopIteration:
-                spaceship = animate_spaceship(canvas, line, column, spaceship_frames)
-                spaceship.send(None)
+                spaceship_frames.append(spaceship_frames.pop(0))
+                spaceship = animate_spaceship(
+                    canvas,
+                    line,
+                    column,
+                    spaceship_frames
+                )
+
+        spaceship.send(None)
         canvas.refresh()
         time.sleep(TIC_TIMEOUT)
 
